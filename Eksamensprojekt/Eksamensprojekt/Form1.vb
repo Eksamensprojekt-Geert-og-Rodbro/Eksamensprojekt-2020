68a07cf8a6f4e8430081f0f39e69220d135d3bc2
Kasper Geert <kasp553c@elev.tradium.dk>
2020-03-30 15:01:43 +0200

    15:01

--- Eksamensprojekt/Eksamensprojekt/Form1.vb
+++ Eksamensprojekt/Eksamensprojekt/Form1.vb
@@ -28,16 +28,32 @@
         Tbox_Sb.Text = "0"
         Tbox_Sc.Text = "0"
     End Sub
-
-    Private Sub Tbox_Va_TextChanged(sender As Object, e As EventArgs) Handles Tbox_Va.TextChanged
-
-    End Sub
-
-    Private Sub But_luk_Click(sender As Object, e As EventArgs) Handles But_luk.Click
-        End
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Pbox_billeder.Image = My.Resources.Clean
     End Sub
 
     Private Sub Tbox_Va_Click(sender As Object, e As EventArgs) Handles Tbox_Va.Click
-        Pbox_billeder.Image = Properties.resour
+        Pbox_billeder.Image = My.Resources.A
+    End Sub
+    Private Sub But_luk_Click(sender As Object, e As EventArgs) Handles But_luk.Click
+        End
+    End Sub
+    Private Sub Tbox_Vb_Click(sender As Object, e As EventArgs) Handles Tbox_Vb.Click
+        Pbox_billeder.Image = My.Resources.B
+    End Sub
+    Private Sub Tbox_Vc_Click(sender As Object, e As EventArgs) Handles Tbox_Vc.Click
+        Pbox_billeder.Image = My.Resources.C
+    End Sub
+
+    Private Sub Tbox_Sa_Click(sender As Object, e As EventArgs) Handles Tbox_Sa.Click
+        Pbox_billeder.Image = My.Resources.a_lille
+    End Sub
+
+    Private Sub Tbox_Sb_Click(sender As Object, e As EventArgs) Handles Tbox_Sb.Click
+        Pbox_billeder.Image = My.Resources.b_lille
+    End Sub
+
+    Private Sub Tbox_Sc_Click(sender As Object, e As EventArgs) Handles Tbox_Sc.Click
+        Pbox_billeder.Image = My.Resources.c_lille
     End Sub
 End Class
